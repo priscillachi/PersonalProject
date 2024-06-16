924f9d1ec26a1393c6daf47802aee8adeb74d9e1
Priscilla Nguyen <chi._nguyen@outlook.com>
2024-06-17 01:42:35 +1000

    created a page to generate films

---
+++ src/main/java/PersonalProject/MovieTimePage.java
@@ -0,0 +1,131 @@
+package PersonalProject;
+
+import org.checkerframework.checker.units.qual.A;
+import processing.core.*;
+import static processing.core.PConstants.*;
+import processing.opengl.*;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
+import processing.event.KeyEvent;
+import processing.event.MouseEvent;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+
+import java.io.*;
+import java.util.*;
+
+import com.jogamp.opengl.GLProfile;
+
+
+public class MovieTimePage extends Page {
+    private ArrayList<String> scienceFictionMovies = new ArrayList<String>();
+    private ArrayList<String> romanceMovies = new ArrayList<String>();
+    private ArrayList<String> childrensMovies = new ArrayList<String>();
+    private ArrayList<String> comedyMovies = new ArrayList<String>();
+    private PImage television;
+    private boolean overHomePageButton;
+
+
+    public MovieTimePage(App app) {
+        super(app);
+    }
+
+    public void setBackground() {
+        this.backgroundImage = app.loadImage(app.getClass().getResource("movie-time-background.jpg").getPath().replace("%20", " "));
+    }
+
+    public void setFont() {
+        this.headerFont = app.createFont(app.getClass().getResource("Newake-Font-Demo.otf").getPath().replace("%20", " "), 100);
+        this.textFont = app.createFont(app.getClass().getResource("WorkSans.ttf").getPath().replace("%20", " "), 100);
+    }
+
+    public void setImages() {
+        this.television = app.loadImage(app.getClass().getResource("television-2.png").getPath().replace("%20", " "));
+    }
+
+    public void setData() {
+
+    }
+
+    public void drawBackground() {
+        app.background(this.backgroundImage);
+        app.image(this.television, 920, 720, 300, 300);
+    }
+
+    public void drawHeader() {
+        app.textFont(this.headerFont);
+        app.stroke(248, 252, 240);
+        app.textSize(150);
+        app.textAlign(CENTER, CENTER);
+        app.fill(248, 252, 240);
+        app.text("MOVIE TIME", 782, 80);
+        app.noStroke();
+    }
+    
+    public void drawDescription() {
+        app.textFont(this.textFont);
+        app.fill(248, 252, 240);
+        app.textSize(20);
+        app.text("This is another shit game to help me when I am feeling indecisive.", 782, 180);
+        app.text("What can I say, I just have the best taste in film xx", 782, 210);
+        app.text("I jest... also, I digress... click one of the buttons on the right to generate a film in that category.", 782, 240);
+    }
+
+    public void drawButtons() {
+        app.fill(211, 211, 211);
+        app.rectMode(CENTER);
+        app.rect(200, 400, 300, 90, 20, 20, 20, 20);
+
+        app.textFont(this.headerFont);
+        app.textSize(45);
+        app.fill(88,57,39);
+        app.text("HOME PAGE", 200, 400);
+    }
+
+    public void checkMouse() {
+
+    }
+
+    public void checkButton() {
+        if (app.mouseX >= 50 && app.mouseX <= 350 && app.mouseY >= 355 && app.mouseY <= 445) {
+            this.overHomePageButton = true;
+        } else {
+            this.overHomePageButton = false;
+        }
+    }
+
+    public void checkClickButton() {
+
+    }
+
+    public void highlightButton() {
+        if (this.overHomePageButton == true) {
+            app.cursor(HAND);
+
+            app.fill(248, 252, 240);
+            app.rectMode(CENTER);
+            app.rect(200, 400, 320, 110, 20, 20, 20, 20);
+    
+            app.textFont(this.headerFont);
+            app.textSize(45);
+            app.fill(88,57,39);
+            app.text("HOME PAGE", 200, 400);
+
+            if (app.clicked == true) {
+                app.currentPage = app.homePage;
+            }
+
+        } else {
+            app.cursor(ARROW);
+        }
+    }
+
+    public void highlightHeader() {
+
+    }
+    public void generation() {
+
+    }
+}
